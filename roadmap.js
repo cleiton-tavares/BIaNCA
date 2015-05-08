ac2ad0af61b910f778aacacabf23dad414932899
Cleiton Tavares <cleitontavares@live.com>
2015-05-08 09:58:09 -0300

    Create roadmap.js

---
+++ roadmap.js
@@ -0,0 +1,21 @@
+var roadmap = { " generico " : [
+  '1. Reconhecimento vocal',
+  '2. Produção de sons',
+  '3. Reconhecimento de ações',
+  '4. Interpretações de ações',
+  '5. Criando respostas basicas',
+  '6. Reconhecimento de hardware',
+  '7. Reconhecimento de funções basicas',
+  '8. Autosuficiencia de entradas de dados',
+  '9. Preprocessamento de resposta basicas a estimulos',
+  '10. Reconhecimento de espaço 3d',
+  '11. Integração com sensores',
+  '12. Reconhecimento de dados de sensores',
+  '13. Resposta basica a estimulos externos',
+  '14. Resposta basica a estimulos visuais',
+  '15. Aplicações de diretrizes basicas sobre o software',
+  '16. Reconhecimento facial',
+  '17. Ações expontaneas baseadas em estimulos',
+  '18. Aprendizado de maquina'
+  ]
+}
